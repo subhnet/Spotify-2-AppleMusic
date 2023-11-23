a338ab1b635053fa4bc668314e9af4b9cff79691
Luka Stärk <luka.staerk@outlook.com>
2023-11-23 18:08:14 +0100

    creates the playlist by with the name from the csv if it does not exists, does not add songs that are allready in the playlist, optionaly use the folder with all the csv-files to create a new playlist for each file.

--- convertsongs.py
+++ convertsongs.py
@@ -21,12 +21,35 @@ def get_connection_data(f,prompt):
     else:
             return input(prompt)
 
+def create_apple_music_playlist(session, playlist_name):
+    url = "https://amp-api.music.apple.com/v1/me/library/playlists"
+    data = {
+        'attributes': {
+            'name': playlist_name,
+            'description': 'A new playlist created via API'
+        }
+    }
+    # test if playlist exists and create it if not
+    response = session.get(url)
+    if response.status_code == 200:
+        for playlist in response.json()['data']:
+            if playlist['attributes']['name'] == playlist_name:
+                print(f"Playlist {playlist_name} already exists!")
+                return playlist['id']
+    response = session.post(url, json=data)
+    if response.status_code == 201:
+        sleep(1.5)
+        return response.json()['data'][0]['id']
+    else:
+        raise Exception(f"Error {response.status_code} while creating playlist {playlist_name}!")
+        return None
+    
 # Getting user's data for the connection
 token = get_connection_data("token.dat", "\nPlease enter your Apple Music Authorization (Bearer token):\n")
 media_user_token = get_connection_data("media_user_token.dat", "\nPlease enter your media user token:\n")
 cookies = get_connection_data("cookies.dat", "\nPlease enter your cookies:\n")
 
-playlist_identifier = input("\nPlease enter the playlist identifier:\n")
+# playlist_identifier = input("\nPlease enter the playlist identifier:\n")
 
 # function to escape apostrophes
 def escape_apostrophes(s):
@@ -102,7 +125,7 @@ def get_itunes_id(title, artist, album):
 
 # Function to add a song to a playlist
 def add_song_to_playlist(session, song_id, playlist_id, playlist_name):
-    try:    
+    try:   
         request = session.post(f"https://amp-api.music.apple.com/v1/me/library/playlists/{playlist_id}/tracks", json={"data":[{"id":f"{song_id}","type":"songs"}]})
         # Checking if the request is successful
         if requests.codes.ok:
@@ -116,9 +139,25 @@ def add_song_to_playlist(session, song_id, playlist_id, playlist_name):
         print(f"HOST ERROR: Apple Music might have blocked the connection during the add of {song_id} to playlist {playlist_name}!\nPlease wait a few minutes and try again.\nIf the problem persists, please contact the developer.")
         return False
 
+def get_playlist_track_ids(session, playlist_id):
+    # test if song is already in playlist
+    try:
+        response = session.get(f"https://amp-api.music.apple.com/v1/me/library/playlists/{playlist_id}/tracks")
+        if response.status_code == 200:
+            #print(response.json()['data'])
+            return [track['attributes']['playParams']['catalogId'] for track in response.json()['data']]
+        elif response.status_code == 404:
+            return []
+        else:
+            raise Exception(f"Error {response.status_code} while getting playlist {playlist_id}!")
+            return None
+    except:
+        raise Exception(f"Error while getting playlist {playlist_id}!")
+        return None
 # Opening session
-with requests.Session() as s:
-    s.headers.update({"Authorization": f"{token}",
+def create_playlist_and_add_song(file):
+    with requests.Session() as s:
+        s.headers.update({"Authorization": f"{token}",
                     "media-user-token": f"{media_user_token}",
                     "Cookie": f"{cookies}",
                     "Host": "amp-api.music.apple.com",
@@ -133,12 +172,16 @@ with requests.Session() as s:
                     "TE": "trailers"})
     
     # Getting the playlist name
-    playlist_name = os.path.basename(argv[1])
+    playlist_name = os.path.basename(file)
     playlist_name = playlist_name.split('.')
     playlist_name = playlist_name[0]
-    
+
+    playlist_identifier = create_apple_music_playlist(s, playlist_name)
+
+    playlist_track_ids = get_playlist_track_ids(s, playlist_identifier)
+    print(playlist_track_ids)
     # Opening the inputed CSV file
-    with open(str(argv[1]), encoding='utf-8') as file:
+    with open(str(file), encoding='utf-8') as file:
         file = csv.reader(file)
         next(file)
         # Initializing variables for the stats
@@ -154,6 +197,11 @@ with requests.Session() as s:
             track_id = get_itunes_id(title, artist, album)
             # If the song is found, add it to the playlist
             if track_id:
+                if str(track_id) in playlist_track_ids:
+                    print(f'\nN°{n} | {title} | {artist} | {album} => {track_id}')
+                    print(f"Song {track_id} already in playlist {playlist_name}!")
+                    failed += 1
+                    continue
                 print(f'\nN°{n} | {title} | {artist} | {album} => {track_id}')
                 sleep(0.5)
                 if add_song_to_playlist(s, track_id, playlist_identifier, playlist_name):
@@ -168,9 +216,21 @@ with requests.Session() as s:
                     f.write('\n')
                 failed += 1
             sleep(1.5)
+    # Printing the stats report
+    print(f'\n - STAT REPORT -\nPlaylist Songs: {n}\nConverted Songs: {converted}\nFailed Songs: {failed}\nPlaylist converted at {round(converted/n*100)}%')
 
-# Printing the stats report
-print(f'\n - STAT REPORT -\nPlaylist Songs: {n}\nConverted Songs: {converted}\nFailed Songs: {failed}\nPlaylist converted at {round(converted/n*100)}%')
+
+if __name__ == "__main__":
+    if len(argv) > 1 and argv[1]:
+        if ".csv" in argv[1]:
+            create_playlist_and_add_song(argv[1])
+        else:
+            # get all csv files in the directory argv[1]
+            files = [f for f in os.listdir(argv[1]) if os.path.isfile(os.path.join(argv[1], f))]
+            # loop through all csv files
+            for file in files:
+                if ".csv" in file:
+                    create_playlist_and_add_song(os.path.join(argv[1], file))
 
 # Developped by @therealmarius on GitHub
 # Based on the work of @simonschellaert on GitHub
